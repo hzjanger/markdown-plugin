9dfc517dd666c4e51a453e3a8cd7f247f8420fe0
hzjanger <1305696249@qq.com>
2020-06-16 20:49:11 +0800

    修改发布tgz包的脚本

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,17 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-ng build --prod
+# 切换为npm镜像,避免上传到npm时出错
+nrm use npm
 
-# 运行Dockerfile文件,创建docker容器
-docker build -f Dockerfile -t my-markdown .
+# 插件打包
+ng build --project z-markdown --prod
 
-# 将创建的容器拷贝一份,并命名为registry.cn-beijing.aliyuncs.com/hezhijian/my-book
-docker tag my-markdown:latest registry.cn-beijing.aliyuncs.com/hezhijian/my-markdown
+# 进入打包文件
+cd dist/z-markdown/
 
-# 上传到阿里云容器镜像服务
-docker push registry.cn-beijing.aliyuncs.com/hezhijian/my-markdown:latest
+# 上传
+npm publish --access public
 
-# 删除创建的容器
-docker image rm my-markdown:latest
+# 切换为淘宝镜像
+nrm use taobao
